fb2e8654a13c442c1bbbfe9437dea77a4263fe56
user <user@DESKTOP-FFAHHEP>
2026-06-03 20:39:26 +0900

    nextintメソットを作り、コードを簡略化しました。

--- Github/src/teamwake.java
+++ Github/src/teamwake.java
@@ -38,12 +38,50 @@ public class teamwake {
 		} else {//if文の終わりとelse文の初め
 			System.out.println("分けるチームと人数にあまり" + keisan_amari + "が出ました。");
 			System.out.println(keisan_amari +"の人は、どうしますか？　１：その人はぬかす　２：一回これで終了して、考え直す　３：小数点で表す");
-			int ifelseint = scanner.nextInt();
+			int ifelseint = 0;
+			try {
+				ifelseint = scanner.nextInt();
+			} catch (Exception e2) {
+				System.err.println("エラーが発生しました！終了してエラー文を表示します．");
+				System.err.println(e2);
+			}
 			switch (ifelseint) {
 			case 1 -> nannninka = nannninka - keisan_amari;
+			case 2 -> System.exit(0);
 			}//switch文の終わり
 			}//if else文の終わり
-		
+		System.out.println("チームの名前を教えてください（省略する場合は、チームA B Cと別れていき、省略する方法は、省略という名前にしてください）");
+		String[] teamname = new String[nanteamka];
+		int menbar1 = nanteamka;
+		int menbar2 = 0;
+		for (;menbar1>1; menbar1--) {
+			System.out.println(menbar1 +"つ目のチームの名前");
+			try {
+			teamname[menbar2] = scanner.nextLine();
+			} catch (Exception e3) { 
+				System.err.println("エラーが発生しました！終了してエラー文を表示します．");
+				System.err.println(e3);
+			}//catch（例外処理）の終わり
+			if (teamname[menbar2].equals("省略")) {
+				System.out.println("チームの名前つけを省略します");
+				break;
+			}//if文の終わり
+			menbar2++;
+		}//for文の終わり
+		System.out.println("次に分ける生徒に名前を付けます。また、これも省略できます。");
+		String[] name = new String[nannninka];
+		int menbar3 = nannninka;
+		int menbar4 = 0;
+		for (;menbar3>1; menbar3--) {
+			System.out.println(menbar3+"つ目の人の名前（または、仮名）");
+			name[menbar4] = scanner.nextLine();
+			menbar4++;
+			if (name[menbar4].equals("省略")) {
+				System.out.println("名前つけを省略します。");
+				break;
+			}//if文の終わり
+		}//for文の終わり
+		System.out.println("では、チーム分けを始めます。しばらくお待ちください。");
 	}//mainメソットの終わり
 	
    public void stoponetime () {
